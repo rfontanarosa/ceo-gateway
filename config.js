ec40085a7751c774d9df2928f6844ae412bc05dd
rfontanarosa <robertofontanarosa@gmail.com>
2019-03-28 14:29:31 +0100

    eslint rules imported by sepal

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-var config = {};
+var config = {}
 
-config.ceo = {};
+config.ceo = {}
 
-config.ceo.url = 'https://127.0.0.1:8080';
-config.ceo.username = 'admin@openforis.org';
-config.ceo.password = '';
-config.ceo.institutionId = '1';
+config.ceo.url = 'https://127.0.0.1:8080'
+config.ceo.username = 'admin@openforis.org'
+config.ceo.password = 'admin'
+config.ceo.institutionId = '1'
 
-module.exports = config;
+module.exports = config
